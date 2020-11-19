8a9e6832b8d08a7611add419a4d0854b19e52bc9
Simone Orsi <simahawk@gmail.com>
2020-11-19 20:29:50 +0100

    base_gs1: cleanup old components

--- base_gs1/components/__init__.py
+++ base_gs1/components/__init__.py
@@ -1,5 +1,1 @@
-from . import common
-from . import output_mixin
-from . import input_mixin
-from . import business_header
-from . import application_receipt_ack
+# from . import application_receipt_ack
