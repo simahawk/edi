22cc79e276db0b1a7ade6b1ccba32a0720948d84
Simone Orsi <simahawk@gmail.com>
2020-11-29 11:48:04 +0100

    edi: test process w/ fake components

--- edi/tests/test_backend_input.py
+++ edi/tests/test_backend_input.py
@@ -5,32 +5,48 @@
 import base64
 
 import mock
-import psycopg2
 from freezegun import freeze_time
 
-from odoo.exceptions import UserError, ValidationError
-from odoo.tests.common import tagged
-from odoo.tools import mute_logger
+from odoo import fields
+from odoo.exceptions import UserError
 
-from .common import EDIBackendCommonTestCase
+from .common import EDIBackendCommonComponentRegistryTestCase
+from .fake_components import FakeInputProcess
+
+# from odoo.tools import mute_logger
 
 
-@tagged("-at_install", "post_install")
-class EDIBackendTestCase(EDIBackendCommonTestCase):
-
-    def test_process_record(self):
+class EDIBackendTestCase(EDIBackendCommonComponentRegistryTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls._build_components(
+            # TODO: test all components lookup
+            cls,
+            FakeInputProcess,
+        )
         vals = {
-            "model": self.partner._name,
-            "res_id": self.partner.id,
-            "edi_exchange_state": "input_received",
+            "model": cls.partner._name,
+            "res_id": cls.partner.id,
             "exchange_file": base64.b64encode(b"1234"),
         }
-        record = self.backend.create_record("test_csv_input", vals)
-        with mock.patch.object(type(self.backend), "_exchange_process") as patch:
-            patch.return_value = "AAA"
-            self.backend.exchange_process(record)
-            patch.assert_called()
-        self.assertEqual(record.edi_exchange_state, "input_processed")
+        cls.record = cls.backend.create_record("test_csv_input", vals)
+
+    def setUp(self):
+        super().setUp()
+        FakeInputProcess.reset_faked()
+
+    def test_process_record(self):
+        self.record.write({"edi_exchange_state": "input_received"})
+        with freeze_time("2020-10-22 10:00:00"):
+            self.record.action_exchange_process()
+        self.assertTrue(FakeInputProcess.check_called_for(self.record))
+        self.assertRecordValues(
+            self.record, [{"edi_exchange_state": "input_processed"}]
+        )
+        self.assertEqual(
+            fields.Datetime.to_string(self.record.exchanged_on), "2020-10-22 10:00:00"
+        )
 
     def test_process_record_with_error(self):
         vals = {
